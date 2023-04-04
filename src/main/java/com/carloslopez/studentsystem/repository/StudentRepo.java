13b0026d0abbc06bf29da1ba2c494a05cccaeba4
Carlos <cjimenez9811@gmail.com>
2023-04-03 18:22:27 -0700

    Established Database Connection

--- src/main/java/com/carloslopez/studentsystem/repository/StudentRepo.java
+++ src/main/java/com/carloslopez/studentsystem/repository/StudentRepo.java
@@ -5,6 +5,7 @@ import com.carloslopez.studentsystem.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/*Spring boot automatically will connect to our mySql database -- and create a student table*/
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Integer> {
 
